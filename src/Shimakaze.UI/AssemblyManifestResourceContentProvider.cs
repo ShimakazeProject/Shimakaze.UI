e64b8b8082a4d03b5ef11e0f2018d81268744567
frg2089 <frg2089@outlook.com>
2023-07-26 13:13:32 +0800

    Load Resources from Assembly
    Signed-off-by: frg2089 <frg2089@outlook.com>

---
+++ src/Shimakaze.UI/AssemblyManifestResourceContentProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+using MimeDetective;
+using MimeDetective.Definitions;
+
+namespace Shimakaze.UI;
+
+public sealed class AssemblyManifestResourceContentProvider : IContentProvider
+{
+    private readonly Assembly _assembly;
+    private readonly IDictionary<string, string> _name;
+    private static readonly ContentInspector Inspector = new ContentInspectorBuilder() {
+        Definitions = Default.All()
+    }.Build();
+
+
+    public AssemblyManifestResourceContentProvider(Assembly assembly)
+    {
+        _assembly = assembly;
+        _name = _assembly.GetManifestResourceNames()
+            .ToDictionary(x => $"/{x.Replace('\\', '/').TrimStart('/')}");
+    }
+
+    public void GetContent(string path, out byte[]? content, out string? contentType)
+    {
+        if (_name.TryGetValue(path, out var name))
+        {
+            using var stream = _assembly.GetManifestResourceStream(name);
+            if (stream is not null)
+            {
+                content = new byte[stream.Length];
+                stream.Read(content, 0, content.Length);
+                var result = Inspector.Inspect(content);
+                contentType = result.ByMimeType().FirstOrDefault()?.MimeType;
+            }
+
+        }
+        throw new NotImplementedException();
+    }
+}
